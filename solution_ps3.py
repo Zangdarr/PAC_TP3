6f17dcdc76bef727203841a090aac30443943e4c
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-26 17:23:47 +0100

    solution_ps3 : récupération du challenge du server.

--- solution_ps3.py
+++ solution_ps3.py
@@ -1,5 +1,13 @@
+from client import Server
+
 if __name__ == '__main__':
         print("RAND resolution starting now...\n\n\n")
         
+        server = Server("http://pac.bouillaguet.info/TP3/PS3/")
+
+        challenge = server.query("PK/verkyndt")
+        
+        print(challenge)
+
 
         print("\n\n\nRAND resolution done\n\n")

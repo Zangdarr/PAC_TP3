bbdcd44d49a6f91be5131b98595b48d1c4207bb2
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-26 18:10:38 +0100

    solution_ps3 : ajout du calcul de la clé non opérationnel.
    Apparament il manque des modulo p car la valeur calculée ne rentre pas dans un int.

--- solution_ps3.py
+++ solution_ps3.py
@@ -19,21 +19,28 @@ if __name__ == '__main__':
 
         msg_1 = "42"
         msg_2 = "93"
-        
+   
         enc1  = signIt(msg_1)
 #        print(enc1)
-
         r1 = enc1[0]
         s1 = enc1[1]
 #        print("r1 : {0}\ns1 : {1}".format(r1,s1))
-
         enc2  = signIt(msg_2)
 #        print(enc1)
-
         r2 = enc2[0]
         s2 = enc2[1]
 #        print("r2 : {0}\ns2 : {1}".format(r2,s2)) 
 
+        k_tmp = (int(msg_1) - int(msg_2)) * (s1- s2)
         
+        k = k_tmp % (int(p)-1)
+#        print(k)
 
+        x1 = (1/k)
+        x2 = int(msg_1)
+        x3 = k * s1
+        x4 = x3 % p
+        x =  x1 * (x2 - x4) % p
+        
+        print(x)
         print("\n\n\nRAND resolution done\n\n")

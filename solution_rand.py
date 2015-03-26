e91d00bceb482a4cf37d1d2bc5dfebb938b85d2e
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-26 17:16:13 +0100

    Adaptation du fichier solution_rand pour utiliser le code C de rand.

--- solution_rand.py
+++ solution_rand.py
@@ -1,14 +1,22 @@
-next = 1
+import ctypes
+from client import Server
 
-def rand(void): # RAND_MAX assumed to be 32767
-    next = next * 1103515245 + 12345
-    return (next/65536) % 32768;
-
-
-def srand( seed ):
-    next = seed
-
+Crandlib = ctypes.CDLL("./Crandlib.so")
 
 if __name__ == '__main__':
         print("RAND resolution starting now...\n\n\n")
+        global Crandlib
+
+        server = Server("http://pac.bouillaguet.info/TP3/rand/")        
+        
+        challenge = server.query("challenge/verkyndt")
+
+        print(challenge)
+
+        
+
+        #dico = { 'key': [0, 42, 666, 1337] }
+
+        #status = server.query("prediction/verkyndt/", dico)
+
         print("\n\n\nRAND resolution done\n\n")

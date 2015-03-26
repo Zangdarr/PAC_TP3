b3f38ec4627530ecc21673ded3da9c6c4b8f11c3
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-26 09:26:07 +0100

    Solution_rand.py : ajout des fonctions utilisé par le language C pour généré un nombre pseudo aléatoire.(retranscrite en python3)

--- solution_rand.py
+++ solution_rand.py
@@ -1,3 +1,14 @@
+next = 1
+
+def rand(void): # RAND_MAX assumed to be 32767
+    next = next * 1103515245 + 12345
+    return (next/65536) % 32768;
+
+
+def srand( seed ):
+    next = seed
+
+
 if __name__ == '__main__':
         print("RAND resolution starting now...\n\n\n")
         print("\n\n\nRAND resolution done\n\n")

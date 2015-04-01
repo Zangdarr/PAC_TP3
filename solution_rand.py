2a8c380a52ef8a394f441901f49981e20d86cb24
Zangdarr <alexandre.verkyndt@gmail.com>
2015-04-01 19:29:56 +0200

    solution_rand : ajout des deux fonction permettant la réalisation du modulo inverse.

--- solution_rand.py
+++ solution_rand.py
@@ -3,6 +3,22 @@ from client import Server
 
 Crandlib = ctypes.CDLL("./Crandlib.so")
 
+def XGCD(a,b):
+  u = (1, 0) #representation de a
+  v = (0, 1) #representation de b
+
+  while b!= 0:
+    q,r = divmod(a, b)
+    a = b
+    b = r
+    tmp = (u[0] - q*v[0], u[1] - q*v[1])
+    u = v
+    v = tmp
+  return a, u[0], u[1]
+
+def modinv(a, b):
+  g, x, y = XGCD(a,b)
+  return x
 if __name__ == '__main__':
         print("RAND resolution starting now...\n\n\n")
         global Crandlib
